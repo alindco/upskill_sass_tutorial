f18015f0e0cf96670ee9ee9a424e395cce1048e3
alindco <akl@alindco.com>
2017-01-04 17:07:07 +0000

    contact controler added send email function call to create

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,1 +1,12 @@
-class ContactMailer < ActionMailer ::ase
+class ContactMailer < ActionMailer ::Base
+  default to: 'akl@alindco.com'
+  
+  def contact_email(name,email,body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: "contact form message")
+    
+    
+  end
+end
