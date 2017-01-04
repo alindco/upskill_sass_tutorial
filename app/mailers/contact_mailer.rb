237222d0bc41f6ea0c2903b71d7aed849e868c0a
alindco <akl@alindco.com>
2017-01-04 22:32:23 +0000

    compleated mail set up for contact form

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,12 @@
-class ContactMailer < ActionMailer ::Base
+class ContactMailer < ActionMailer::Base
   default to: 'akl@alindco.com'
   
-  def contact_email(name,email,body)
+  def contact_email(name, email, body)
     @name = name
     @email = email
     @body = body
-    mail(from: email, subject: "contact form message")
+    
+    mail(from: email, subject: 'contact form message')
     
     
   end
