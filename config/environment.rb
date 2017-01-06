3f7e0ed23f405cf7e4651e956a16eb7a9fe0ddaf
alindco <akl@alindco.com>
2017-01-06 17:19:23 +0000

    corrected mailer setting to config environment file

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,22 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActiveMailer::base.smtp_settings = {
-  :port       => ENV['MAILGUN_SMTP_PORT'],
-  :address       => ENV['MAILGUN_SMTPT_SERVER'],
+ActionMailer::Base.smtp_settings = {
+  :port           => ENV['MAILGUN_SMTP_PORT'],
+  :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain       =>'akl-saas.herokuapp.com',
-  :authentication      => plain,
+  :domain         => 'akl-saas.herokuapp.com',
+  :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
+
+# ActiveMailer::base.smtp_settings = {
+#   :port       => ENV['MAILGUN_SMTP_PORT'],
+#   :address       => ENV['MAILGUN_SMTPT_SERVER'],
+#   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+#   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+#   :domain       =>'akl-saas.herokuapp.com',
+#   :authentication      => plain,
+# }
+# ActionMailer::Base.delivery_method = :smtp
